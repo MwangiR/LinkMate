809f1a80a1fd091d54c8d7242d8b472667ae267f
Renny <rennymwangi@gmail.com>
2023-09-25 02:10:19 +0930

    Defined user,thought and reaction schema

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
